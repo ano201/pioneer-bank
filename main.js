5c57707879bac1d0bef69cf6ff1ad7cd8bbad666
ano201 <ahmu689.com>
2021-07-20 12:19:49 +0600

    Mission failed successfully

--- main.js
+++ main.js
@@ -6,4 +6,47 @@ login.addEventListener("click", function() {
   transactionArea.setAttribute("style", "display : block")
 })
 
-//-----------chaos---------
+
+//-----------chaos------
+
+
+//-----depposit button
+
+const depositBtn = document.getElementById("deposit-button");
+depositBtn.addEventListener("click", function() {
+  const inputDeposit = toNumber("input-deposit");
+  updateAmount("current-deposit", inputDeposit);
+  updateAmount("current-ballance", inputDeposit);
+})
+
+
+//------withdraw button
+
+const withdrawButton = document.getElementById("withdraw-button");
+withdrawButton.addEventListener("click", function() {
+  
+  const inputWithdraw = toNumber("input-withdraw");
+  updateAmount("current-withdraw", inputWithdraw);
+  updateAmount("current-ballance", inputWithdraw*-1)
+})
+
+
+//-----string to number function
+
+function toNumber(id) {
+  const input = document.getElementById(id).value;
+  const number = parseFloat(input);
+  document.getElementById(id).value = "";
+  return number;
+}
+
+
+
+//-----update amount function
+
+function updateAmount(currentAmountId, inputNumber) {
+  const currentAmount = document.getElementById(currentAmountId).innerHTML;
+  const amountNumber = parseFloat(currentAmount);
+  const totalAmount = amountNumber + inputNumber;
+  document.getElementById(currentAmountId).innerHTML = totalAmount;
+}
